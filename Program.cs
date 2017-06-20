ccf33cb136b6156f6e20966746d8c33239a2ccf2
Andrew Carbungco <andrew.carbungco@gmail.com>
2017-06-19 20:27:52 -0700

    Add files via upload
    Created a "hello world" project in Visual Studio 2017

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a key to exit");
+            Console.ReadKey();
+        }
+    }
+}
